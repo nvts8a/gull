b1eec8aec29509c17d88438602080526ffe27ebb
Steven Justin <nvts8a@gmail.com>
2015-04-26 19:06:19 -0500

    Setup dev Larid proxy

---
+++ server/proxies/larid.js
@@ -0,0 +1,15 @@
+var proxyPath = '/larid';
+
+module.exports = function(app) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+  var proxy = require('http-proxy').createProxyServer({});
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    proxy.web(req, res, { target: 'http://localhost:3000' });
+  });
+};
