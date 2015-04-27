fa2a672b977add1330e074005c6c7222061af240
Steven Justin <nvts8a@gmail.com>
2015-04-26 20:10:33 -0500

    Transtion to home during registration

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
 export default Ember.Controller.extend({
+  actions: {
+    createUser: function() {
+      this.transitionToRoute("home");      
+    }
+  }
 });
