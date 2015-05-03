a0f376d83664cdba3bb7ed40c52268e6bc42533c
Steven Justin <nvts8a@gmail.com>
2015-05-03 14:35:01 -0500

    [#10] Added landing route with login and reg

---
+++ app/controllers/landing.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    loginUser: function() {
+      this.transitionToRoute("home");      
+    },
+    createAndLoginUser: function() {
+      this.transitionToRoute("home");      
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -1,13 +1,14 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 export default Router.map(function() {
-  this.route('registration');
-  this.route('home');
-  this.route('login');
-  this.route('activity');
+  this.route("landing", { path: "/" });
+  this.route("registration");
+  this.route("home");
+  this.route("login");
+  this.route("activity");
 });
