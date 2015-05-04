6b2a945d00d4c28596364b3904d25a38848f0bdc
Steven Justin <nvts8a@gmail.com>
2015-05-03 22:15:01 -0500

    [#5] Loging and reg set a session token

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -3,10 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     loginUser: function() {
+      setSessionToken(this.getProperties("email"));
       this.transitionToRoute("home");      
     },
     createAndLoginUser: function() {
+      setSessionToken(this.getProperties("email"));
       this.transitionToRoute("home");      
     }
   }
 });
+
+var setSessionToken = function(token) {
+  localStorage.setItem('session:token', token);
+}
