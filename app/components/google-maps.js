8179942f28ef833f1a947592d39a46f2fd535356
Steven Justin <nvts8a@gmail.com>
2015-05-03 22:03:00 -0500

    [#1] Now places a marker where maps centers

--- app/components/google-maps.js
+++ app/components/google-maps.js
@@ -5,15 +5,17 @@ export default Ember.Component.extend({
   
   insertMap: function() {
     var container = this.$(".map-canvas");
-
+    var myLatLng = new google.maps.LatLng( localStorage.getItem("geoposition:latitude"), localStorage.getItem("geoposition:longitude"));
     var options = {
-      center: new google.maps.LatLng(
-        localStorage.getItem("geoposition:latitude"),
-        localStorage.getItem("geoposition:longitude")),
+      center: myLatLng,
       zoom: 17,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
-    new google.maps.Map(container[0], options);
+    var map = new google.maps.Map(container[0], options);
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: "Hello Sarah!"
+    });
   }.on("didInsertElement")
 });
