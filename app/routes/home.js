0852547258e488cae5bfa6c17b4a3e4437bcdb9b
Steven Justin <nvts8a@gmail.com>
2015-05-16 08:12:50 -0500

    [#24] Moved BigVideo init to routes, home route disposes

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  init: function() {
+    var bigVideo = Gull.get("bigVideo");
+    if(bigVideo) {
+      bigVideo.dispose();
+    }
+  }
 });
--- app/routes/landing.js
+++ app/routes/landing.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  init: function() {
+    var bigVideo = new $.BigVideo();
+    bigVideo.init();
+    bigVideo.show('http://vjs.zencdn.net/v/oceans.mp4', {ambient: true});
+    Gull.set("bigVideo", bigVideo);
+  }
 });
