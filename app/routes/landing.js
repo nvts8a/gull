a7b3a38f9f1688f503dd6c199fd7198420229909
Steven Justin <nvts8a@gmail.com>
2015-05-17 14:14:43 -0500

    [#14] Added own video, instead of BigVideo default

--- app/routes/landing.js
+++ app/routes/landing.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   init: function() {
     var bigVideo = new $.BigVideo();
     bigVideo.init();
-    bigVideo.show('http://vjs.zencdn.net/v/oceans.mp4', {ambient: true});
+    bigVideo.show('gull-video.mp4', {ambient: true});
     Gull.set("bigVideo", bigVideo);
   }
 });
