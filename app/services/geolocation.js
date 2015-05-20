1bb978b6bded2e7afaeba345ddaa0c5af39942d9
Steven Justin <nvts8a@gmail.com>
2015-05-20 00:43:00 -0500

    [#32] Geolocation service integration

---
+++ app/services/geolocation.js
@@ -0,0 +1,9 @@
+import ajax from "ic-ajax";
+
+export function getGeolocation() {
+  var url = "larid/geolocation"
+            +"?latitude="+localStorage.getItem("geoposition:latitude")
+            +"&longitude="+localStorage.getItem("geoposition:longitude");
+
+  return ajax({ url: url, type: "GET" });
+}
